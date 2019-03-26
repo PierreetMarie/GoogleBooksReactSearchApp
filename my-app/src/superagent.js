5e9d37e9544cfa692826898d49d33be569942c6d
Enrique Sandino <enrique.sandino1000@gmail.com>
2019-03-25 21:59:28 -0400

    googleSearchApp

---
+++ my-app/src/superagent.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+
+// const SuperAgent = () => {
+//     return(
+//         <header>
+//             <i className="fas fa-book fa-2x"></i>
+//             <h1>Book Cards</h1>
+//         </header>
+//     )
+// }
+
+
+// export default SuperAgent;
